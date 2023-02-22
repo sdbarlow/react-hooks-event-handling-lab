d65c6b730c7f9f88f6224527b4a9db34f3f63216
sdbarlow <sethdbarlow@gmail.com>
2023-02-22 11:26:01 -0700

    all tests pass

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    return(
+        <button onBlur={() => console.log('Hey! Eyes on me!')} onFocus={() => console.log('Good!')}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
